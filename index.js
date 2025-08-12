4e770082f54c9ad1397c11ac003c2addf49d71bd
Chloe Mills <chloecmills@gmail.com>
2025-08-12 15:02:56 +0100

    made an event listener on the search button

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
 let usersInput = [];
 
-searchInput.addEventListener("click", function () {
-  console.log("Clicked");
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("clicked button");
+
+  const searchMovie = searchInput.value;
+  console.log("Searched Movies: ", searchMovie);
 
   fetch("http://www.omdbapi.com/?i=tt3896198&apikey=4a11e4f7")
     .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+    .then((data) => console.log(data));
 });
