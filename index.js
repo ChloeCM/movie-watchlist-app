711025457dc95474d96bd5ce9512f0542e99b108
Chloe Mills <chloecmills@gmail.com>
2025-08-12 18:20:25 +0100

    Updated javascript to display the films to the DOM

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
+let mainContainer = document.getElementById("main-container");
 let usersInput = [];
 
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("clicked button");
 
   const searchMovie = searchInput.value;
   console.log("Searched Movies: ", searchMovie);
 
-  fetch("http://www.omdbapi.com/?i=tt3896198&apikey=4a11e4f7")
+  if (!searchMovie) {
+    console.log("Please type something");
+    mainContainer.innerHTML = `<p>Please type a movie title</p>`;
+    return;
+  }
+
+  usersInput.push(searchMovie);
+
+  fetch(`https://www.omdbapi.com/?apikey=4a11e4f7&s=${searchMovie}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+
+      console.log(data.Response);
+      if (data.Response === "False" || !data.Search) {
+        mainContainer.innerHTML = `<p>${data.Error || "No results found."}</p>`;
+        return;
+      }
+      renderFilms(data.Search); // pass the array of results
+    })
+    .catch(() => {
+      mainContainer.innerHTML = "<p>Network error. Try again.</p>";
+    });
 });
+
+function renderFilms(results) {
+  const html = results
+    .map(
+      (film) => `
+      <article class="film" data-id="${film.imdbID}">
+        <img src="${film.Poster} alt="${film.Title} />
+        <h3>${film.Title}</h3>
+        <p>${film.Year}</p>
+        <button data-id="${film.imdbID}">Add to Watchlist</button>
+      </article>
+  `
+    )
+    .join("");
+
+  mainContainer.innerHTML = `<section class="results">${html}</section>`;
+}
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=4a11e4f7
